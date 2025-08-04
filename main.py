980f54beacf7abb921b1217d42f781ece187d2c7
Jricci0098 <jricci18@gmail.com>
2025-08-03 20:16:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from flask import Flask, request
+from app.bot import handle_message_payload
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "GroupMe Novena Bot is running"
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    data = request.get_json()
+    if data and data.get("sender_type") != "bot":  # Prevent bot from responding to itself
+        handle_message_payload(data)
+    return "OK", 200
 
